4690df7166d9a8e10351ee9ec7243faf8886e940
wesleykg <wesley.gerelle@gmail.com>
2016-04-28 11:15:29 -0700

    working download script; very slow

--- scripts/download.py
+++ scripts/download.py
@@ -1,24 +1,26 @@
 '''Usage: retrieve.py <wanted_species>'''
 
-from docopt import docopt # For command-line arguments
+#from docopt import docopt # For command-line arguments
 import requests
+from requests.auth import HTTPDigestAuth
+import bz2
 
-cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
+#cmdln_args = docopt(__doc__) # Creates a dictionary of command-line arguments
 
-wanted_species_file = cmdln_args.get('<wanted_species>')
+wanted_species_file = "../data/wanted_species.txt"# cmdln_args.get('<wanted_species>')
 
 prefix = "http://onekp.westgrid.ca/1kp-data/"
 infix = "/assembly/"
 suffix = "-SOAPdenovo-Trans-assembly.fa.bz2"
-
-URL_list = []
+outname_suffix = "-SOAPdeno-Trans-assembly.fa"
+data_dir = '../data/'
 
 with open(wanted_species_file, 'r') as species_file:
     for line in species_file:
         ID = line.split('-')[0]
         URL = prefix + line.rstrip() + infix + ID + suffix
-        URL_list.append(URL)
-
-for URL in URL_list:
-    assembly = requests.get(URL, auth=('1kp-data', '1kp-rna1'))
-    print assembly.text
+        compressed_assembly = requests.get(URL, 
+                                auth=HTTPDigestAuth('1kp-data', '1kp-rna1'))
+        assembly = bz2.decompress(compressed_assembly.content)
+        with open(data_dir + ID + outname_suffix, 'w') as out_assembly:
+            out_assembly.write(assembly)
