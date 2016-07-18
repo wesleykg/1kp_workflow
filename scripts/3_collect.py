45303756d6ad304c102b7fef80da6c6f9419b36a
Wesley Gerelle <wesley@Wesleys-iMac.lan>
2016-07-18 11:02:47 -0700

    added comments

--- scripts/3_collect.py
+++ scripts/3_collect.py
@@ -1,35 +1,66 @@
-import pandas
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
+'''Usage 3_collect.py <all_assemblies> <blast_results>'''
+
+# Modules
+import pandas  # Reading in csv blast results
+from Bio import SeqIO  # Indexing all scaffolds
+# from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord  # Producing empty SeqRecords
 
 
-all_assemblies_filename = '../data/all_assemblies_cleaned.fasta'
-blast_results_filename = '../data/PHYPA_atpI_blast-results.csv'
+# Check if running interactively in an iPython console, or in a script
+# from the command-line
+def in_ipython():
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
+# Run in a script from the command-line
+if in_ipython() is False:
+    from docopt import docopt  # Command-line argument handler
+    cmdln_args = docopt(__doc__)
+    all_assemblies_filename = cmdln_args.get('<all_assemblies>')
+    blast_results_filename = cmdln_args.get('<blast_results>')
+# Run interatively in an iPython console
+if in_ipython() is True:
+    all_assemblies_filename = '../data/all_assemblies_cleaned.fasta'
+    blast_results_filename = '../data/PHYPA_atpI_blast-results.csv'
 
+# Index the concatenated file of all 1kp assemblies of interest
 all_scaffolds = SeqIO.index(all_assemblies_filename, format='fasta')
 
+# Names of each column in the csv blast results file
 table_header = 'query', 'blast_db', 'scaf', 'query_len', 'scaf_len', \
                 'align_len', 'e_val', 'blast_ver', 'seq'
 
-wanted_scaffolds = []
-
+# Read in the csv blast results file
 blast_results = pandas.read_csv(blast_results_filename, names=table_header)
 
-missing_results = blast_results[blast_results.scaf == 'None found']
-missing_scaffold_names = missing_results.blast_db
-
-for name in missing_scaffold_names:
-    missing_seq = Seq('')
-    missing_seq_record = SeqRecord(missing_seq, id=name, description='')
-    wanted_scaffolds.append(missing_seq_record)
-
+# Retrieve results for blast searches that found a scaffold
 hit_results = blast_results[blast_results.scaf != 'None found']
-wanted_scaffold_names = hit_results.scaf
 
+# Retrieve results for blast searches that didn't find a scaffold
+missing_results = blast_results[blast_results.scaf == 'None found']
+
+# Initalize list of wanted scaffolds to write to file
+wanted_scaffolds = []
+
+# Loop through each scaffold name and search for the accompanying SeqRecord
+# in the index of all scaffolds. Add each matched SeqRecord to the list of
+# wanted scaffolds
+wanted_scaffold_names = hit_results.scaf
 for name in wanted_scaffold_names:
     name = name.rstrip()
     wanted_scaffold = all_scaffolds[name]
     wanted_scaffolds.append(wanted_scaffold)
+
+# Retrieve 1kp species IDs for blast searches with no scaffolds found. Loop
+# through this list of ID's and create an empty SeqRecord for each ID
+# missing a scaffold
+missing_scaffold_names = missing_results.blast_db
+for name in missing_scaffold_names:
+    missing_seq = Seq('')
+    missing_seq_record = SeqRecord(missing_seq, id=name, description='')
+    wanted_scaffolds.append(missing_seq_record)
 
 SeqIO.write(wanted_scaffolds, 'wanted_scaffolds.fasta', format='fasta')
